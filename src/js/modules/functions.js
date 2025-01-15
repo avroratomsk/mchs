c4324b3f90cae7dcacab1d267525398dd83ceed3
sanchyopas <saniagolovanev@gmail.com>
2025-01-15 17:41:23 +0700

    fix

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -16,3 +16,70 @@ export function isWebp() {
 		document.documentElement.classList.add(className);
 	});
 }
+
+const burger = document.getElementById("burger");
+
+burger?.addEventListener("click", () => {
+	document.querySelector(".nav").classList.add("_active");
+})
+
+const navClose = document.getElementById("nav-close");
+
+navClose?.addEventListener("click", () => {
+	document.querySelector(".nav").classList.remove("_active");
+})
+
+// Блокировка скролла
+export const bodyLock = (e) => {
+	let widthScrollBar = window.innerWidth - document.documentElement.clientWidth;
+
+	// document.querySelector('.header').style.paddingRight = widthScrollBar + 'px';
+	// document.documentElement.style.marginRight = widthScrollBar + 'px';
+	document.body.classList.add('_lock');
+}
+
+// Удаление блокировки скролла
+export const bodyUnLock = (e) => {
+	// document.documentElement.style.marginRight = '0px';
+	// document.querySelector('.header').style.paddingRight = '0px';
+	document.body.classList.remove('_lock');
+}
+
+const openPopup = (e) => {
+	bodyLock();
+	const currentPopup = document.getElementById(e.currentTarget.dataset.popup);
+	currentPopup.classList.add("popup_show");
+
+	const hiddenField = currentPopup.querySelector("#order-product");
+	if (hiddenField) {
+		hiddenField.value = e.currentTarget.dataset.name;
+	}
+};
+
+const closePopup = (e) => {
+	bodyUnLock();
+	document.querySelector(".popup_show").classList.remove("popup_show");
+};
+
+document.addEventListener("keydown", (e) => {
+	if (e.keyCode === 27 && document.querySelector(".popup_show").classList.contains("popup_show")) {
+		document.querySelector(".popup_show").classList.remove("popup_show");
+		document.body.classList.remove("_lock");
+		bodyUnLock();
+	}
+});
+
+const popup = document.querySelectorAll(".popup");
+popup?.forEach(popup => popup.addEventListener("click", (e) => {
+	if (!e.target.closest(".popup__content")) {
+		e.currentTarget.classList.remove("popup_show");
+		document.body.classList.remove("_lock");
+		bodyUnLock();
+	}
+}));
+
+const modalOpenButtonsList = document.querySelectorAll("[data-popup]");
+modalOpenButtonsList?.forEach(btn => btn.addEventListener("click", openPopup));
+
+const modalCloseButtonsList = document.querySelectorAll("[data-close]");
+modalCloseButtonsList?.forEach(btn => btn.addEventListener("click", closePopup));
