0c38c8971ce0c648e959e5339ae0bc3488e3fe90
sanchyopas <saniagolovanev@gmail.com>
2025-02-27 16:43:12 +0700

    fix: image single page

--- src/js/modules/calendary.js
+++ src/js/modules/calendary.js
@@ -1,5 +1,3 @@
-console.log("work")
-
 const events = [{
   "date": "2025-01-20 15:31:28",
   "title": "Какое-то событие"
@@ -31,56 +29,60 @@ function displayDates() {
   const today = new Date(); // Текущая дата
   const dateList = document.getElementById('date-list'); // Элемент для вставки дат
 
-  // Очистим список (на случай, если вызываем функцию повторно)
-  dateList.innerHTML = '';
-  const days = [];
+  if(dateList) {
+    // Очистим список (на случай, если вызываем функцию повторно)
+    dateList.innerHTML = '';
+    const days = [];
 
-  // Добавляем текущий день и шесть следующих
-  for (let i = 0; i < 9; i++) {
-    const currentDate = new Date();
-    currentDate.setDate(today.getDate() + i); // Прибавляем i дней
-    days.push(formatDate(currentDate)); // Сохраняем форматированные даты
-  }
-
-  const calendaryInfo = document.getElementById("calendary-info");
-  calendaryInfo.innerHTML = "";
-  // Обрабатываем каждый день и проверяем наличие событий
-  days.forEach((day) => {
-    const listItem = document.createElement('div'); // Создаем элемент списка
-    listItem.className = "calendary-day__item";
-    listItem.setAttribute("data-id", day.split(' ')[0]);
-    const spanListItem = document.createElement('span');
-    spanListItem.textContent = day.split(' ')[0].split('-')[2]; // Устанавливаем текст дня
-    listItem.appendChild(spanListItem);
-
-    // Проверяем наличие события на этот день
-    const eventForDay = events.find(event => event.date.startsWith(day.split(' ')[0]));
-    if (eventForDay) {
-      const star = document.createElement('span');
-      star.className = 'calendary-day__start'
-      star.textContent = '★'; // Символ звезды
-      star.style.color = 'red'; // Можно изменить цвет
-      listItem.appendChild(star); // Добавляем звезду к дате
-
-
-      // Добавляем описание события под датой
-      const eventDescription = document.createElement('div');
-      eventDescription.classList.add('calendary-info__day');
-      eventDescription.setAttribute("id", day.split(' ')[0]);
-      eventDescription.textContent = eventForDay.title; // Заголовок события
-      calendaryInfo.appendChild(eventDescription); // Добавляем описание события
-    } else {
-      // Если событий нет, добавляем сообщение "нет событий"
-      const noEventMessage = document.createElement('div');
-      noEventMessage.classList.add('calendary-info__day');
-      noEventMessage.setAttribute("id", day.split(' ')[0]);
-      noEventMessage.textContent = 'нет событий';
-      calendaryInfo.appendChild(noEventMessage); // Добавляем текст "нет событий"
+    // Добавляем текущий день и шесть следующих
+    for (let i = 0; i < 9; i++) {
+      const currentDate = new Date();
+      currentDate.setDate(today.getDate() + i); // Прибавляем i дней
+      days.push(formatDate(currentDate)); // Сохраняем форматированные даты
     }
-    dateList.appendChild(listItem); // Добавляем в список
-  });
-  document.querySelector(".calendary-day__item").classList.add("active");
-  document.querySelector(".calendary-info__day").classList.add("active");
+
+    const calendaryInfo = document.getElementById("calendary-info");
+    if(calendaryInfo) {
+      calendaryInfo.innerHTML = "";
+      // Обрабатываем каждый день и проверяем наличие событий
+      days.forEach((day) => {
+        const listItem = document.createElement('div'); // Создаем элемент списка
+        listItem.className = "calendary-day__item";
+        listItem.setAttribute("data-id", day.split(' ')[0]);
+        const spanListItem = document.createElement('span');
+        spanListItem.textContent = day.split(' ')[0].split('-')[2]; // Устанавливаем текст дня
+        listItem.appendChild(spanListItem);
+
+        // Проверяем наличие события на этот день
+        const eventForDay = events.find(event => event.date.startsWith(day.split(' ')[0]));
+        if (eventForDay) {
+          const star = document.createElement('span');
+          star.className = 'calendary-day__start'
+          star.textContent = '★'; // Символ звезды
+          star.style.color = 'red'; // Можно изменить цвет
+          listItem.appendChild(star); // Добавляем звезду к дате
+
+
+          // Добавляем описание события под датой
+          const eventDescription = document.createElement('div');
+          eventDescription.classList.add('calendary-info__day');
+          eventDescription.setAttribute("id", day.split(' ')[0]);
+          eventDescription.textContent = eventForDay.title; // Заголовок события
+          calendaryInfo.appendChild(eventDescription); // Добавляем описание события
+        } else {
+          // Если событий нет, добавляем сообщение "нет событий"
+          const noEventMessage = document.createElement('div');
+          noEventMessage.classList.add('calendary-info__day');
+          noEventMessage.setAttribute("id", day.split(' ')[0]);
+          noEventMessage.textContent = 'нет событий';
+          calendaryInfo.appendChild(noEventMessage); // Добавляем текст "нет событий"
+        }
+        dateList.appendChild(listItem); // Добавляем в список
+      });
+      document.querySelector(".calendary-day__item").classList.add("active");
+      document.querySelector(".calendary-info__day").classList.add("active");
+    }
+  }
 }
 
 // Вызов функции при загрузке страницы
